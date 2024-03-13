5aae484cb3638b9273af3d6d5c3292dba0f3c995
R Kanishkasree <124056440+KanishkasreeR@users.noreply.github.com>
2024-03-13 22:29:51 +0530

    index.js

--- index.js
+++ index.js
@@ -1,34 +1,67 @@
+// require("dotenv").config({ path: "./config.env" });
+// const express = require("express");
+// const mongoose =  require('mongoose');
+// const body_parser = require('body-parser')
+// const auth = require("./auth.js");
+// const app = express();
+
+
+
+// app.use(body_parser.json());
+// app.use(require("cors")());
+// // app.options('*', cors());
+
+
+// app.use("/api", require("./authentication.js"));
+// app.use("/api", require("./contact1.js"));
+
+
+// async function connectDB(){
+//     try{
+//             await mongoose.connect('mongodb+srv://kanishka:Aqpfk15rpTGS578W@cluster05.pgwmpx4.mongodb.net/ContactManager?retryWrites=true&w=majority&appName=Cluster05')
+//             const port = process.env.PORT || 8000
+//             console.log('DB Connection Established')
+//             app.listen(port,function(){
+//                 console.log(`Listening on port ${port}`)
+//             })
+//         }
+      
+//     catch(error){
+//        console.log(error)
+//        console.log("Can't Establish Connection")
+//     }
+//     }
+//     connectDB()
+
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
-const mongoose =  require('mongoose');
-const body_parser = require('body-parser')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require("cors");
 const auth = require("./auth.js");
+
 const app = express();
 
-
-
-app.use(body_parser.json());
-app.use(require("cors")());
-// app.options('*', cors());
-
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api", require("./authentication.js"));
 app.use("/api", require("./contact1.js"));
 
+const PORT = process.env.PORT || 8000;
 
-async function connectDB(){
-    try{
-            await mongoose.connect('mongodb+srv://kanishka:Aqpfk15rpTGS578W@cluster05.pgwmpx4.mongodb.net/ContactManager?retryWrites=true&w=majority&appName=Cluster05')
-            const port = process.env.PORT || 8000
-            console.log('DB Connection Established')
-            app.listen(port,function(){
-                console.log(`Listening on port ${port}`)
-            })
-        }
-      
-    catch(error){
-       console.log(error)
-       console.log("Can't Establish Connection")
+async function connectDB() {
+    try {
+        await mongoose.connect('mongodb+srv://kanishka:Aqpfk15rpTGS578W@cluster05.pgwmpx4.mongodb.net/ContactManager?retryWrites=true&w=majority&appName=Cluster05');
+        console.log('DB Connection Established');
+        app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+        console.log("Can't Establish Connection");
     }
-    }
-    connectDB()
+}
+
+connectDB();
+
